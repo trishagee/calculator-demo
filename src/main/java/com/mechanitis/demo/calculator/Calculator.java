8756dd82eb20722084067562764e0eaf8fd937e7
Trisha Gee <trisha.gee@gmail.com>
2020-10-08 13:19:35 +0200

    Initial Commit. Very basic implementation of a calculator, with passing tests

---
+++ src/main/java/com/mechanitis/demo/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.mechanitis.demo.calculator;
+
+public class Calculator {
+    public int add(int first, int second) {
+        return first + second;
+    }
+}
---
+++ src/test/java/com/mechanitis/demo/calculator/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.mechanitis.demo.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    void shouldAddIntegers() {
+        // given
+        Calculator calc = new Calculator();
+
+        // when
+        int result = calc.add(2, 3);
+
+        // then
+        assertEquals(5, result);
+    }
+}
